ab140149fb1358415caea76f7efc6eaa2b8da22f
AdemarSs <ademar-ss@hotmail.com>
2018-12-21 14:16:03 -0300

    Ralizado construtor(es), equals e hashcode e relacionamento - Endereco / CidadeRalizado construtor(es), equals e hashcode e relacionamento - Endereco / Cidade
    git status
    .
    
    git
    git status
    ''

--- src/main/java/com/fieb/senai/entidades/Estado.java
+++ src/main/java/com/fieb/senai/entidades/Estado.java
@@ -1,14 +1,95 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.fieb.senai.entidades;
 
-/**
- *
- * @author Home
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ESTADO")
 public class Estado {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @ElementCollection
+    @CollectionTable(name = "CIDADE")
+    private List<Cidade> cidade = new ArrayList<>();
+
+    public Estado(Integer id, String nome) {
+        this.id = null;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Cidade> getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(List<Cidade> cidade) {
+        this.cidade = cidade;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.nome);
+        hash = 29 * hash + Objects.hashCode(this.cidade);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Estado other = (Estado) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.cidade, other.cidade)) {
+            return false;
+        }
+        return true;
+    }
+    
+        
 }
