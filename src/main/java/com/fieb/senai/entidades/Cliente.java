1fc1b323bd1cd8d0e838089911b844f33cb29af2
AdemarSs <ademar-ss@hotmail.com>
2018-12-21 13:50:22 -0300

    Ralizado  construtor(es), equals e hashcode e relacionamento cliente, endereco e sexo

--- src/main/java/com/fieb/senai/entidades/Cliente.java
+++ src/main/java/com/fieb/senai/entidades/Cliente.java
@@ -1,14 +1,145 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.fieb.senai.entidades;
 
-/**
- *
- * @author Home
- */
+import com.fieb.senai.entidades.enums.Sexo;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CLIENTE")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Integer id;
+
+    @Column(name = "NOME")
+    private String nome;
+
+    @Column(name = "IDADE")
+    private Integer idade;
     
+    @Column(name = "SEXO")
+    private Sexo sexo;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "ENDERECO_ID")
+    private Endereco endereco;
+
+    @ElementCollection
+    @CollectionTable(name = "TELEFONE")
+    private Set<String> telefones = new HashSet<>();
+
+    public Cliente(String nome, Integer idade, Sexo sexo) {
+        this.id = null;
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;        
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public Set<String> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(Set<String> telefones) {
+        this.telefones = telefones;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 97 * hash + Objects.hashCode(this.id);
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        hash = 97 * hash + Objects.hashCode(this.idade);
+        hash = 97 * hash + Objects.hashCode(this.sexo);
+        hash = 97 * hash + Objects.hashCode(this.endereco);
+        hash = 97 * hash + Objects.hashCode(this.telefones);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (!Objects.equals(this.idade, other.idade)) {
+            return false;
+        }
+        if (this.sexo != other.sexo) {
+            return false;
+        }
+        if (!Objects.equals(this.endereco, other.endereco)) {
+            return false;
+        }
+        if (!Objects.equals(this.telefones, other.telefones)) {
+            return false;
+        }
+        return true;
+    }          
+
 }
--- src/main/java/com/fieb/senai/entidades/enums/Sexo.java
+++ src/main/java/com/fieb/senai/entidades/enums/Sexo.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.fieb.senai.entidades.enums;
 
-/**
- *
- * @author Home
- */
-public class Sexo {
-    
+public enum Sexo {
+
+    MASCULINO,
+    FEMININO;
+
 }
